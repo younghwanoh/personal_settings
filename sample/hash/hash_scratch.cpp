1ee631bc268e9fce20e9edc3d5f6c8a9c61d1e19
garion9013 <garion9013@gmail.com>
2015-09-16 16:49:28 +0900

    minor samples

---
+++ sample/hash/hash_scratch.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+
+using namespace std;
+
+class HashEntry {
+private:
+      int key;
+      int value;
+public:
+      HashEntry(int key, int value) {
+            this->key = key;
+            this->value = value;
+      }
+ 
+      int getKey() {
+            return key;
+      }
+ 
+      int getValue() {
+            return value;
+      }
+};
+
+const int TABLE_SIZE = 128;
+ 
+class HashMap {
+private:
+      HashEntry **table;
+public:
+      HashMap() {
+            table = new HashEntry*[TABLE_SIZE];
+            for (int i = 0; i < TABLE_SIZE; i++)
+                  table[i] = NULL;
+      }
+ 
+      int get(int key) {
+            int hash = (key % TABLE_SIZE);
+            while (table[hash] != NULL && table[hash]->getKey() != key)
+                  hash = (hash + 1) % TABLE_SIZE;
+            if (table[hash] == NULL)
+                  return -1;
+            else
+                  return table[hash]->getValue();
+      }
+ 
+      void put(int key, int value) {
+            int hash = (key % TABLE_SIZE);
+            while (table[hash] != NULL && table[hash]->getKey() != key)
+                  hash = (hash + 1) % TABLE_SIZE;
+            if (table[hash] != NULL)
+                  delete table[hash];
+            table[hash] = new HashEntry(key, value);
+      }
+ 
+      ~HashMap() {
+            for (int i = 0; i < TABLE_SIZE; i++)
+                  if (table[i] != NULL)
+                        delete table[i];
+            delete[] table;
+      }
+};
+
+int main(){
+  HashMap hash;
+  hash.put(12,707);
+  hash.put(140,709);
+  hash.put(13,808);
+  cout<< hash.get(12) << endl;
+	cout<< hash.get(140) << endl;
+  cout<< hash.get(13) << endl;
+  return 0;
+}
