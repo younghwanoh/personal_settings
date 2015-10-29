918b09e4cc7ad9ed4d009c24f6974e0bfb3d1d3c
Young H. Oh <garion9013@gmail.com>
2015-10-29 17:53:24 +0900

    deprecate sample

--- sample/JavaScript/worker.js
+++
@@ -1,7 +0,0 @@
-onmessage = function(message){
-    // get messages through message obj(e.g. message.data)
-    for (var i=0;i<500;i++){
-       // Do something
-    }
-    postMessage();
-}
--- sample/OpenCL/offline/kernel.cl
+++
@@ -1,8 +0,0 @@
-//#pragma OPENCL EXTENSION cl_khr_spir : enable
-
-__kernel void vecAdd(__global float* a)
-{
-    int gid = get_global_id(0);
-
-    a[gid] += a[gid];
-}
