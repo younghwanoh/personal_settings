089800abf88e5f5fd5483b089fe94538ac841d83
Young H. Oh <garion9013@gmail.com>
2018-11-28 19:38:53 +0900

    minor

--- script/example_slackmessage.py
+++ script/example_slackmessage.py
@@ -1,11 +1,13 @@
 import re
 import os
 
-command = []
+config = []
                 # cand_max / do_dyn_cand. /  thresh / sel_size / do_dyn_sel /  thresh
-# command.append([120,          "False",          12.8,    60,        "False",     12.8])
-# command.append([320,          "False",          12.8,    10,        "False",     12.8])
-command.append([320,          "False",          12.8,    "None",    "True",      12.8])
+config.append([40,          "False",          12.8,     20,        "False",     12.8])
+config.append([80,          "False",          12.8,     20,        "False",     12.8])
+config.append([160,         "False",          12.8,     20,        "False",     12.8])
+
+messages="All one score experiment - kvmemn2n"
 
 def sub(key, value):
     modified = re.sub(r'%s = .*' % (key), r'%s = %s' % (key, value), txt)
@@ -20,7 +22,7 @@ def dyn_or_static(do_dyn_cand, do_dyn_sel):
 with open("param.py", "r") as f:
     global txt
     txt = f.read()
-    for (cand_max, do_dyn_cand, cth, sel_size, do_dyn_sel, sth) in command:
+    for (cand_max, do_dyn_cand, cth, sel_size, do_dyn_sel, sth) in config:
         txt = sub("do_dynamic_candidate", do_dyn_cand)
         txt = sub("candidate_max_budget", cand_max)
         txt = sub("dynamic_candidate_threshold", cth)
@@ -28,17 +30,19 @@ with open("param.py", "r") as f:
         txt = sub("do_dynamic_selection", do_dyn_sel)
         txt = sub("selection_size", sel_size)
         txt = sub("dynamic_selection_threshold", sth)
+
+        txt = sub("min_aware_iteration", cand_max)
         # print(txt)
         # print("-----------------------------------------------")
-        # Get characterization string
-        char = dyn_or_static(do_dyn_cand, do_dyn_sel)
-        char = "light_weight"
 
-        # Configuration file
+        # Characterization string
+        char = dyn_or_static(do_dyn_cand, do_dyn_sel)
+        char = "negative"
         with open("param.py", "wb") as fw:
             fw.write(txt)
 
-        filename = "log_{}".format("_".join(
+        # Set filename from config
+        filename = "bert_{}".format("_".join(
           [char, str(cand_max), str(sel_size), do_dyn_cand, do_dyn_sel, "sample1500", "new"]
         ).lower())
 
@@ -47,4 +51,4 @@ with open("param.py", "r") as f:
         os.system(" ".join([command, filename]))
 
         # Send messages to slack
-        os.system("slackmessage `cat {}`".format(filename))
+        os.system("slackmessage \"`cat {}`\"".format(filename))
